08e3c0bf9399492ef79cb3dcd11f468d14d1b7a5
Chori <yidodeveloper@gmail.com>
2024-01-07 19:57:58 +0900

    DB CRUD restAPI 구현 중

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [msg, setMsg] = useState([]);
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setMsg(data);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {msg.map((content, idx) => (
+            <li key={`${idx} - ${content}`}>{content}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
