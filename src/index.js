c9ffb485a98847eecca004c1f9761697b7d79730
Chori <yidodeveloper@gmail.com>
2024-02-09 23:09:45 +0900

    20240209 axios에서 baseURL 직접 설정하지 않도록 설정

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 
 // 배포 환경에서 baseURL 설정 안됨 -> 문제 없는지 조사
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-console.log(process.env.REACT_APP_API_URL);
+// axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+// console.log(process.env.REACT_APP_API_URL);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
