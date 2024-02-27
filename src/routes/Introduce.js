2a0514c0b3ed9b6160cd102b6f87fc4b50a0137e
Chori <yidodeveloper@gmail.com>
2024-02-28 01:08:23 +0900

    20240228 소개 페이지 레이아웃 조정

--- src/routes/Introduce.js
+++ src/routes/Introduce.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { Button, Image } from "react-bootstrap";
+import { Button, Col, Image, Row } from "react-bootstrap";
 import styles from "../css/Introduce.module.css";
 import checkRoutineImage from "../images/home_check_routine.png";
 import manageImage from "../images/modify_routine.png";
@@ -30,19 +30,17 @@ function Introduce() {
       </Button>
 
       <section className={styles.mission}>
-        <p className={styles.mission_title}>MISSION</p>
-        <p className={styles.mission_korean}>나를 키우는 공간</p>
-        <p className={styles.mission_english}>
-          A space where you grow yourself
-        </p>
+        <p className={styles.missionTitle}>MISSION</p>
+        <p className={styles.missionKorean}>나를 키우는 공간</p>
+        <p className={styles.missionEnglish}>A space where you grow yourself</p>
       </section>
 
       <section className={styles.identity}>
-        <p className={styles.identity_subtitle}>IDENTITY</p>
-        <p className={styles.identity_title}>
+        <p className={styles.identitySubtitle}>IDENTITY</p>
+        <p className={styles.identityTitle}>
           여러분은 어떤 사람이 되고 싶나요?
         </p>
-        <p className={styles.identity_explain}>
+        <p className={styles.identityExplain}>
           미라클 농장을 시작하기 전, 꼭 '나는 어떤 사람이 되고 싶은지'
           고민해보세요. <br />
           <br />
@@ -58,57 +56,79 @@ function Introduce() {
       <section>
         <div className={styles.functionTitle}>FUNCTION</div>
         <div className={styles.checkRoutine}>
-          <div className={styles.checkRoutineExplain}>
-            <p className={styles.checkRoutineExplainSubtitle}>FUNCTION 1.</p>
-            <p className={styles.checkRoutineExplainTitle}>루틴 확인 및 인증</p>
-            <p className={styles.checkRoutineExplainMain}>
-              나와 다른 사람들의 루틴을 확인하고 <br />
-              사진을 촬영해 루틴 실천을 인증하세요
-            </p>
-          </div>
-          <div className={styles.checkRoutineImageContainer}>
-            <img
-              className={styles.checkRoutineImage}
-              src={checkRoutineImage}
-              alt="루틴 확인 화면"
-            />
-          </div>
+          <Row className={styles.row}>
+            <Col className={styles.imageCol}>
+              <div className={styles.checkRoutineImageContainer}>
+                <img
+                  className={styles.checkRoutineImage}
+                  src={checkRoutineImage}
+                  alt="루틴 확인 화면"
+                />
+              </div>
+            </Col>
+            <Col className={styles.explainCol}>
+              <div className={styles.checkRoutineExplain}>
+                <p className={styles.checkRoutineExplainSubtitle}>
+                  FUNCTION 1.
+                </p>
+                <p className={styles.checkRoutineExplainTitle}>
+                  루틴 확인 및 인증
+                </p>
+                <p className={styles.checkRoutineExplainMain}>
+                  나와 다른 사람들의 루틴을 확인하고 <br />
+                  사진을 촬영해 루틴 실천을 인증하세요
+                </p>
+              </div>
+            </Col>
+          </Row>
         </div>
 
         <div className={styles.manageRoutine}>
-          <div className={styles.manageExplain}>
-            <p className={styles.manageExplainSubtitle}>FUNCTION 2.</p>
-            <p className={styles.manageExplainTitle}>루틴 관리</p>
-            <p className={styles.manageExplainMain}>
-              수정하고 싶은 루틴이 있다면 <br />
-              요일/시간, 인증방법, 활성화 여부를 선택하세요
-            </p>
-          </div>
-          <div className={styles.manageImageContainer}>
-            <img
-              className={styles.manageImage}
-              src={manageImage}
-              alt="루틴 관리 화면"
-            />
-          </div>
+          <Row className={styles.row}>
+            <Col className={styles.explainCol}>
+              <div className={styles.manageExplain}>
+                <p className={styles.manageExplainSubtitle}>FUNCTION 2.</p>
+                <p className={styles.manageExplainTitle}>루틴 관리</p>
+                <p className={styles.manageExplainMain}>
+                  수정하고 싶은 루틴이 있다면 <br />
+                  요일/시간, 인증방법, 활성화 여부를 선택하세요
+                </p>
+              </div>
+            </Col>
+            <Col className={styles.imageCol}>
+              <div className={styles.manageImageContainer}>
+                <img
+                  className={styles.manageImage}
+                  src={manageImage}
+                  alt="루틴 관리 화면"
+                />
+              </div>
+            </Col>
+          </Row>
         </div>
 
         <div className={styles.statistics}>
-          <div className={styles.statisticsExplain}>
-            <p className={styles.statisticsExplainSubtitle}>FUNCTION 3.</p>
-            <p className={styles.statisticsExplainTitle}>루틴 현황</p>
-            <p className={styles.statisticsExplainMain}>
-              자신의 루틴 현황을 <br />
-              월별, 요일별, 루틴별로 살펴보세요
-            </p>
-          </div>
-          <div className={styles.statisticsImageContainer}>
-            <img
-              className={styles.statisticsImage}
-              src={statisticsImage}
-              alt="루틴 현황 화면"
-            />
-          </div>
+          <Row className={styles.row}>
+            <Col className={styles.imageCol}>
+              <div className={styles.statisticsImageContainer}>
+                <img
+                  className={styles.statisticsImage}
+                  src={statisticsImage}
+                  alt="루틴 현황 화면"
+                />
+              </div>
+            </Col>
+            <Col className={styles.explainCol}>
+              <div className={styles.statisticsExplain}>
+                <p className={styles.statisticsExplainSubtitle}>FUNCTION 3.</p>
+                <p className={styles.statisticsExplainTitle}>루틴 현황</p>
+                <p className={styles.statisticsExplainMain}>
+                  자신의 루틴 현황을 <br />
+                  월별, 요일별, 루틴별로 살펴보세요
+                </p>
+              </div>
+            </Col>
+          </Row>
         </div>
       </section>
 
